6bbe773fb7d80ffd0eff4a3dd1eec0d01e683a2f
Daniel Buckle <danielbuckle31@gmail.com>
2013-11-21 13:30:58 +0000

    New Changes
    Decoupled the program massively. Now running only one DB connector at
    one time

--- src/domain/TeamStatus.java
+++ src/domain/TeamStatus.java
@@ -1,6 +1,11 @@
 package domain;
 
 public enum TeamStatus {
+<<<<<<< HEAD
     atRisk, atHighRisk, fairlySafeForNow, definitelySafeForNow, equalButTop, equalOnEverything, champion,
     chanceOfChampionsLeague, chanceOfEuropaLeague, champions, relegated;
+=======
+    notSafe, equal, safe, relegated
+
+>>>>>>> a86130f9a685fd78cc6b1e8bfe559ff7c2745746
 }
--- src/ui/LeagueTable.java
+++ src/ui/LeagueTable.java
@@ -3,6 +3,10 @@ package ui;
 import domain.LeagueVerdict;
 import domain.Team;
 import domain.TeamStatus;
+<<<<<<< HEAD
+=======
+import domain.Verdict;
+>>>>>>> a86130f9a685fd78cc6b1e8bfe559ff7c2745746
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -11,8 +15,13 @@ import java.awt.*;
 
 public class LeagueTable extends JTable {
 
+<<<<<<< HEAD
     public static final Dimension INITIAL_SIZE = new Dimension(800, 340);
     private LeagueVerdict leagueVerdict;
+=======
+    public static final Dimension INITIAL_SIZE = new Dimension(500, 300);
+    private static final int smallerColumnWidths = 5;
+>>>>>>> a86130f9a685fd78cc6b1e8bfe559ff7c2745746
     DefaultTableModel model;
     JTable table;
 
@@ -20,12 +29,47 @@ public class LeagueTable extends JTable {
 
         leagueVerdict = new LeagueVerdict();
         model = new DefaultTableModel();
+<<<<<<< HEAD
         model.setColumnIdentifiers(new String[]{"Team Name", "Goal Diff", "Points", "Played", "LeagueVerdict"});
         table = new JTable(model){
             @Override
             public boolean isCellEditable(int row, int column) {
                 return false;
         }};
+=======
+        model.setColumnIdentifiers(new String[]{"Team", "GoalDiff", "Pts", "Played", "Verdict"});
+        table = new JTable(model){@Override
+                                  public boolean isCellEditable(int row, int column) {
+            return false;
+        }};
+
+        TableColumn colGD = table.getColumnModel().getColumn(1);
+        colGD.setPreferredWidth(smallerColumnWidths);
+
+        TableColumn colPts = table.getColumnModel().getColumn(2);
+        colPts.setPreferredWidth(smallerColumnWidths);
+
+        TableColumn colGms = table.getColumnModel().getColumn(3);
+        colGms.setPreferredWidth(smallerColumnWidths);
+
+        for(Team team: leagueSQLConnector.teamList){
+            model.addRow(new Object[]{team.getName(), team.getGoalDifference(),
+                    team.getPoints(), team.getGamesPlayed()});
+        }
+
+        int i = 0;
+        while(i < 20){
+            if(verdict.verdictArray.get(i) == TeamStatus.equal)
+                model.setValueAt("Drawing", i, 4);
+            else if(verdict.verdictArray.get(i) == TeamStatus.safe)
+                model.setValueAt("Safe", i, 4);
+            else if(verdict.verdictArray.get(i) == TeamStatus.notSafe)
+                model.setValueAt("Not Safe", i, 4);
+            else
+                model.setValueAt("Relegation Zone", i, 4);
+            i++;
+        }
+>>>>>>> a86130f9a685fd78cc6b1e8bfe559ff7c2745746
 
         JScrollPane pane = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
                 JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
